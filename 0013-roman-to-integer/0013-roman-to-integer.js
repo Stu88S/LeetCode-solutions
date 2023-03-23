2c05f3c6937459305fcdf223a7378c6862a25eee
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-03-23 15:17:40 +0300

    Time: 126 ms (73.48%), Space: 47.7 MB (35.46%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const romanToInt = s => {
+    const romanValues = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+        const currentSymbolValue = romanValues[s[i]];
+        const nextSymbolValue = romanValues[s[i + 1]];
+
+        if (nextSymbolValue && currentSymbolValue < nextSymbolValue) {
+            result -= currentSymbolValue;
+        } else {
+            result += currentSymbolValue;
+        }
+    }
+    return result;
+};
