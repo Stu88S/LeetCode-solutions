4a5bd02aed2afd7cc1cb1bd6b56e39d2479df001
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-03-23 15:29:39 +0300

    Time: 55 ms (94.31%), Space: 41.7 MB (93.95%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -4,16 +4,14 @@
  */
 function longestCommonPrefix(strs) {
   if (strs.length === 0) {
-    return '';
+    return "";
   }
-  let prefix = strs[0];
-  for (let i = 1; i < strs.length; i++) {
-    while (strs[i].indexOf(prefix) !== 0) {
-      prefix = prefix.substring(0, prefix.length - 1);
-      if (prefix === '') {
-        return '';
-      }
+  
+  return strs.reduce((prefix, str) => {
+    let i = 0;
+    while (i < prefix.length && i < str.length && prefix[i] === str[i]) {
+      i++;
     }
-  }
-  return prefix;
+    return prefix.slice(0, i);
+  });
 }
