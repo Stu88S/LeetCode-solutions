02dd0ec2d88a12e43219af5a98c4a6101b943387
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-03-27 21:09:29 +0300

    Time: 61 ms (64.28%), Space: 41.8 MB (82.97%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -12,13 +12,18 @@ const merge = (nums1, m, nums2, n) => {
 
   while (i >= 0 && j >= 0) {
     if (nums1[i] > nums2[j]) {
-      nums1[k--] = nums1[i--];
+      nums1[k] = nums1[i];
+      i--;
     } else {
-      nums1[k--] = nums2[j--];
+      nums1[k] = nums2[j];
+      j--;
     }
+    k--;
   }
 
   while (j >= 0) {
-    nums1[k--] = nums2[j--];
+    nums1[k] = nums2[j];
+    j--;
+    k--;
   }
 };
