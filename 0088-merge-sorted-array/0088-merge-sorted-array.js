1bdbaf10fa86fa30b0af59b2556adbc3e2b4f0b8
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-03-27 20:58:48 +0300

    Time: 58 ms (78.21%), Space: 42.1 MB (56.68%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -5,25 +5,14 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-function merge(nums1, m, nums2, n) {
-  let i = m - 1;
-  let j = n - 1;
-  let k = m + n - 1;
-  
-  while (i >= 0 && j >= 0) {
-    if (nums1[i] >= nums2[j]) {
-      nums1[k] = nums1[i];
-      i--;
-    } else {
-      nums1[k] = nums2[j];
-      j--;
-    }
-    k--;
+const merge = (nums1, m, nums2, n) => {
+  let p1 = m - 1;
+  let p2 = n - 1;
+  let idx = m + n - 1;
+
+  while (p1 >= 0 && p2 >= 0) {
+    nums1[idx--] = nums1[p1] > nums2[p2] ? nums1[p1--] : nums2[p2--];
   }
-  
-  while (j >= 0) {
-    nums1[k] = nums2[j];
-    j--;
-    k--;
-  }
-}
+
+  nums1.splice(0, p2 + 1, ...nums2.slice(0, p2 + 1));
+};
