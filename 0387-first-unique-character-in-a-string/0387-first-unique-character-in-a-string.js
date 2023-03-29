489aa9d54b0a5c89d2a5a2558d81cd19f9023085
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-03-29 21:19:49 +0300

    Time: 95 ms (83.39%), Space: 45.4 MB (56.56%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -3,16 +3,10 @@
  * @return {number}
  */
 function firstUniqChar(str) {
-    let set = new Set();
-    let map = new Map();
-    
-    for (let i = 0; i< str.length; i++) {
-        if(!set.has(str[i])) {
-            set.add(str[i]);
-            map.set(str[i], i);   
-        } else {
-            map.delete(str[i])
-        }
+  for (let i = 0; i < str.length; i++) {
+    if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) {
+      return i;
     }
-    return map.size === 0 ? -1 : map.values().next().value;
+  }
+  return -1;
 }
