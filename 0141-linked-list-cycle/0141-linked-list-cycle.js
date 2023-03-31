8c5ad52ac21b533582c20974ad76b588823f25de
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-03-31 22:53:16 +0300

    Time: 69 ms (85.77%), Space: 44.9 MB (51.69%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const hasCycle = function(head) {
+  // Use two pointers, moving at different speeds, to detect a cycle
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+};
