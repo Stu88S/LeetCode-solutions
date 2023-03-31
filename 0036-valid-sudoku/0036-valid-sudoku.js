625ff469cb1768d14e27aeb473e0121aa2200452
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-03-31 23:13:01 +0300

    Time: 69 ms (90.83%), Space: 44.9 MB (71.62%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.js
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -2,35 +2,31 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-const isValidSudoku = function(board) {
-    let rows = {};
-    let cols = {};
-    let boxes = {};
-
-    for (let i = 0; i < 9; i++) {
-        rows[i] = {};
-        cols[i] = {};
-        boxes[i] = {};
-    }
-
-    for (let r = 0; r < 9; r++) {
-        for (let c = 0; c < 9; c++) {
-            if (board[r][c] !== '.') {
-                let val = board[r][c];
-                let boxIndex = parseInt((r / 3)) * 3 + parseInt(c / 3);
-
-                if (rows[r][val] ||
-                    cols[c][val] ||
-                    boxes[boxIndex][val]) {
-                    return false;
-                }
-
-                rows[r][val] = true;
-                cols[c][val] = true;
-                boxes[boxIndex][val] = true;
-            }
+function isValidSudoku(board) {
+    const rowSet = new Set();
+    const colSet = new Set();
+    const boxSet = new Set();
+    
+    for(let i=0; i<9; i++) {
+        for(let j=0; j<9; j++) {
+            let num = board[i][j];
+            
+            if(num === '.') continue;
+            
+            if(rowSet.has(`${i}-${num}`)) return false;
+            
+            rowSet.add(`${i}-${num}`);
+            
+            if(colSet.has(`${j}-${num}`)) return false;
+            
+            colSet.add(`${j}-${num}`);
+            
+            let boxIndex = 3 * Math.floor(i / 3) + Math.floor(j / 3);
+            if(boxSet.has(`${boxIndex}-${num}`)) return false;
+            
+            boxSet.add(`${boxIndex}-${num}`);
         }
     }
 
     return true;
-};
+}
