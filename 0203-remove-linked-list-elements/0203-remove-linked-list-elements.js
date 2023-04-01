ed4b612de90242aeb6fb7ebb4d6005ca0696dba1
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-04-01 22:34:28 +0300

    Time: 93 ms (9.78%), Space: 46.8 MB (36.59%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+function removeElements(head, val) {
+  if (head === null) {
+    return null;
+  }
+  
+  let current = head;
+  let previous = null;
+  
+  while (current !== null) {
+    if (current.val === val) {
+      if (previous === null) {
+        head = current.next;
+      } else {
+        previous.next = current.next;
+      }
+    } else {
+      previous = current;
+    }
+    
+    current = current.next;
+  }
+  
+  return head;
+}
