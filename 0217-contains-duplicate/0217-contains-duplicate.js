d2cc1484ad6421b66e7cc070cf5b782218afba66
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-04-08 22:53:34 +0300

    Time: 87 ms (84.83%), Space: 53.3 MB (69.35%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,7 +2,14 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-function containsDuplicate(nums) {
-  let set = new Set(nums);
-  return set.size !== nums.length;
-}
+const containsDuplicate = function(nums) {
+  const numSet = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    if (numSet.has(nums[i])) {
+      return true;
+    } else {
+      numSet.add(nums[i]);
+    }
+  }
+  return false;
+};
