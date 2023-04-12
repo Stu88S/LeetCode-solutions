d1fc7b50dc72849556a1578d4b2ee3e1bd3eafe3
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-04-12 21:24:34 +0300

    Time: 64 ms (43.15%), Space: 42.4 MB (24.59%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -7,7 +7,21 @@ const searchMatrix = (matrix, target) => {
   if (matrix.length === 0 || matrix[0].length === 0) {
     return false;
   }
-  const flattened = matrix.flat();
-  const index = flattened.indexOf(target);
-  return index !== -1;
+  const numRows = matrix.length;
+  const numCols = matrix[0].length;
+  let row = 0;
+  let col = numCols - 1;
+
+  while (row < numRows && col >= 0) {
+    const val = matrix[row][col];
+    if (val === target) {
+      return true;
+    } else if (val < target) {
+      row++;
+    } else {
+      col--;
+    }
+  }
+
+  return false;
 };
