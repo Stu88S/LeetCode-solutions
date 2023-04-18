598975f9f49d8f356f7b6c345e855f8c30e3d42d
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-04-18 19:22:29 +0300

    Time: 70 ms (56.80%), Space: 45.6 MB (30.04%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -10,12 +10,13 @@
  * @param {TreeNode} root
  * @return {number}
  */
-function maxDepth(root) {
-  if (root === null) {
-    return 0;
-  } else {
+const maxDepth = function(root) {
+    if (root === null) {
+        return 0;
+    }
+
     const leftDepth = maxDepth(root.left);
     const rightDepth = maxDepth(root.right);
+
     return Math.max(leftDepth, rightDepth) + 1;
-  }
-}
+};
