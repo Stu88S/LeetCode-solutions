c4ef4c68c3c31a47a4342336911c25500aaea7f2
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-04-22 20:37:21 +0300

    Time: 65 ms (78.96%), Space: 45.6 MB (49.12%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNumber = function(nums) {
+    let result = 0;
+    
+    for (let num of nums) {
+        result ^= num;
+    }
+    
+    return result;
+};
