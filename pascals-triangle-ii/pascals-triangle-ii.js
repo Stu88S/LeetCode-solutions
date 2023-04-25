5d9cca3ca97176e072cd579ce7a3a99c7ed2884c
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-04-25 18:29:23 +0300

    Time: 56 ms (73.57%) | Memory: 41.9 MB (64.79%) - LeetSync

--- pascals-triangle-ii/pascals-triangle-ii.js
+++ pascals-triangle-ii/pascals-triangle-ii.js
@@ -6,9 +6,21 @@ function getRow(rowIndex) {
   const result = new Array(rowIndex + 1).fill(0);
   result[0] = 1;
 
+  const halfRowLength = Math.floor((rowIndex + 1) / 2);
+
   for (let i = 1; i <= rowIndex; i++) {
-    for (let j = i; j > 0; j--) {
+    for (let j = Math.min(i, halfRowLength); j > 0; j--) {
       result[j] = result[j] + result[j - 1];
+    }
+  }
+
+  if (rowIndex % 2 === 0) {
+    for (let i = halfRowLength + 1, j = halfRowLength - 1; i <= rowIndex; i++, j--) {
+      result[i] = result[j];
+    }
+  } else {
+    for (let i = halfRowLength, j = halfRowLength - 1; i <= rowIndex; i++, j--) {
+      result[i] = result[j];
     }
   }
 
