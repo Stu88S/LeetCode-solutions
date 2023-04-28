30fa53a99c90f5a86a695be8b4cb8ccf16f789f3
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-04-28 18:46:42 +0300

    Time: 98 ms (86.50%) | Memory: 48.8 MB (79.31%) - LeetSync

--- merge-intervals/merge-intervals.js
+++ merge-intervals/merge-intervals.js
@@ -3,22 +3,19 @@
  * @return {number[][]}
  */
 function merge(intervals) {
-	if (intervals.length === 0) return [];
+  if (intervals.length === 0) return [];
 
-	intervals.sort((a, b) => a[0] - b[0]);
+  intervals.sort((a, b) => a[0] - b[0]);
 
-	const result = [intervals[0]];
+  return intervals.reduce((result, currentInterval) => {
+    const lastIntervalInResult = result[result.length - 1];
 
-	for (let i = 1; i < intervals.length; i++) {
-		const currentInterval = intervals[i];
-		const lastIntervalInResult = result[result.length - 1];
+    if (currentInterval[0] <= lastIntervalInResult[1]) {
+      lastIntervalInResult[1] = Math.max(currentInterval[1], lastIntervalInResult[1]);
+    } else {
+      result.push(currentInterval);
+    }
 
-		if (currentInterval[0] <= lastIntervalInResult[1]) {
-			lastIntervalInResult[1] = Math.max(currentInterval[1], lastIntervalInResult[1]);
-		} else {
-			result.push(currentInterval);
-		}
-	}
-
-	return result;
+    return result;
+  }, [intervals[0]]);
 }
