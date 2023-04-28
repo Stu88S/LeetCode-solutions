1fd5e8d9b72b1ecbcb5e7bca47921dc310c64c14
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-04-28 19:04:35 +0300

    Time: 62 ms (41.71%) | Memory: 42 MB (60.36%) - LeetSync

--- rotate-image/rotate-image.js
+++ rotate-image/rotate-image.js
@@ -3,17 +3,15 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 function rotate(matrix) {
-	const n = matrix.length;
+  const n = matrix.length;
 
-	for (let i = 0; i < n; i++) {
-		for (let j = i + 1; j < n; j++) {
-			[matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
-		}
-	}
+  matrix.forEach((row, i) => {
+    for (let j = i + 1; j < n; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  });
 
-	for (let i = 0; i < n; i++) {
-		for (let j = 0; j < n / 2; j++) {
-			[matrix[i][j], matrix[i][n - j - 1]] = [matrix[i][n - j - 1], matrix[i][j]];
-		}
-	}
+  matrix.forEach((row, i) => {
+    row.reverse();
+  });
 }
