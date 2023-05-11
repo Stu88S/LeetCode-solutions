8d4b15d1f1c8d6564d3ae30116eaf0ae8af8bcca
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-05-11 21:03:59 +0300

    Time: 74 ms (45.82%) | Memory: 43.4 MB (41.46%) - LeetSync

--- function-composition/function-composition.js
+++ function-composition/function-composition.js
@@ -3,17 +3,5 @@
  * @return {Function}
  */
 function compose(functions) {
-    return function(x) {
-        for (let i = functions.length - 1; i >= 0; i--) {
-            x = functions[i](x);
-        }
-        return x;
-    }
+    return functions.reduceRight((prevFn, currentFn) => (value) => currentFn(prevFn(value)), (x) => x);
 }
-
-
-
-/**
- * const fn = compose([x => x + 1, x => 2 * x])
- * fn(4) // 9
- */
