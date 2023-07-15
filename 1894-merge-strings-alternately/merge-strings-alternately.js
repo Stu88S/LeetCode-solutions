f7888b09a2237ce01dfc0ce425d4c8b493df5260
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-07-15 19:32:34 +0300

    Time: 61 ms (49.17%) | Memory: 44.4 MB (5.01%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.js
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -4,18 +4,9 @@
  * @return {string}
  */
 const mergeAlternately = function (word1, word2) {
-	const mergedString = [];
 	const maxLength = Math.max(word1.length, word2.length);
 
-	for (let i = 0; i < maxLength; i++) {
-		if (i < word1.length) {
-			mergedString.push(word1[i]);
-		}
-
-		if (i < word2.length) {
-			mergedString.push(word2[i]);
-		}
-	}
+	const mergedString = Array.from({ length: maxLength }).flatMap((_, i) => [word1[i] || "", word2[i] || ""]);
 
 	return mergedString.join("");
 };
