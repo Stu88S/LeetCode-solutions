1dc5395b239f8bcbfd9154d76490b611326fbd64
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-07-15 22:27:10 +0300

    Time: 61 ms (61.15%) | Memory: 42 MB (87.91%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.js
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.js
@@ -4,7 +4,16 @@
  * @return {string}
  */
 function gcdOfStrings(str1, str2) {
-    if (str1 + str2 !== str2 + str1) return '';
-    const gcd = (a, b) => (0 === b ? a : gcd(b, a % b));
-    return str1.substring(0, gcd(str1.length, str2.length));
+    const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+    const gcdLength = gcd(str1.length, str2.length);
+    const gcdString = str1.substring(0, gcdLength);
+  
+    let str1Check = gcdString.repeat(str1.length / gcdLength);
+    let str2Check = gcdString.repeat(str2.length / gcdLength);
+  
+    if (str1Check === str1 && str2Check === str2) {
+        return gcdString;
+    } else {
+        return "";
+    }
 }
