5a6e3d4dc0271b9c9b5827433a2e672f48ff5b56
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-07-25 16:21:00 +0300

    Time: 74 ms (14.18%) | Memory: 44.3 MB (34.76%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.js
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.js
@@ -4,24 +4,5 @@
  */
 
 const reverseWords = function (s) {
-	let wordsArray = [];
-	let word = "";
-
-	for (let i = 0; i < s.length; i++) {
-		if (s[i] !== " ") {
-			word += s[i];
-		}
-		if ((s[i] === " " || i === s.length - 1) && word) {
-			wordsArray.push(word);
-			word = "";
-		}
-	}
-	let reversed = "";
-	for (let i = wordsArray.length - 1; i >= 0; i--) {
-		reversed += wordsArray[i];
-		if (i > 0) {
-			reversed += " ";
-		}
-	}
-	return reversed;
+	return s.trim().split(/\s+/).reverse().join(" ");
 };
