e6dc1a53347934fe3c00f1db68e22bb817557f58
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-07-28 14:49:44 +0400

    Time: 73 ms (99.99%) | Memory: 54.3 MB (59.02%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -2,26 +2,20 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const productExceptSelf = function(nums) {
-    const length = nums.length;
+const productExceptSelf = function (nums) {
+	const length = nums.length;
 
-    const leftProduct = new Array(length);
-    const rightProduct = new Array(length);
-    const output = new Array(length);
+	const output = new Array(length);
 
-    leftProduct[0] = 1;
-    for (let i = 1; i < length; i++) {
-        leftProduct[i] = nums[i - 1] * leftProduct[i - 1];
-    }
+	output[0] = 1;
+	for (let i = 1; i < length; i++) {
+		output[i] = nums[i - 1] * output[i - 1];
+	}
 
-    rightProduct[length - 1] = 1;
-    for (let i = length - 2; i >= 0; i--) {
-        rightProduct[i] = nums[i + 1] * rightProduct[i + 1];
-    }
-
-    for (let i = 0; i < length; i++) {
-        output[i] = leftProduct[i] * rightProduct[i];
-    }
-
-    return output;
+	let rightProduct = 1;
+	for (let i = length - 1; i >= 0; i--) {
+		output[i] = output[i] * rightProduct;
+		rightProduct *= nums[i];
+	}
+	return output;
 };
