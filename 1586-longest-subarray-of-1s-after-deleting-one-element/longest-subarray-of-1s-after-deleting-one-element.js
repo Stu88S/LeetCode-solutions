f550276b157390c382b2d043cf1f94b37670a288
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-10-28 22:52:46 +0400

    Time: 39 ms (100.00%) | Memory: 47.7 MB (31.63%) - LeetSync

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function longestSubarray(nums) {
+    let left = 0, right = 0, zeroCount = 0, maxLength = 0;
+    
+    while (right < nums.length) {
+        if (nums[right] === 0) zeroCount++;
+            
+        while (zeroCount > 1) {
+            if (nums[left] === 0) zeroCount--;
+            left++;          
+        }
+        maxLength = Math.max(maxLength, right - left);
+        right++;
+    }
+    return maxLength;
+}
