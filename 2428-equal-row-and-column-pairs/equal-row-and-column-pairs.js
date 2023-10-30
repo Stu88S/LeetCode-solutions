df8cc779d2c4032d5db179bf1930892f64de0edd
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-10-30 17:59:29 +0400

    Time: 98 ms (82.11%) | Memory: 50.8 MB (86.09%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.js
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.js
@@ -4,17 +4,20 @@
  */
 function equalPairs(grid) {
 	let count = 0;
+	let map = new Map();
+
+	function arrayToString(array) {
+		return JSON.stringify(array);
+	}
+
 	for (let i = 0; i < grid.length; i++) {
-		for (let j = 0; j < grid.length; j++) {
-			let flag = true;
-			for (let k = 0; k < grid.length; k++) {
-				if (grid[i][k] != grid[k][j]) {
-					flag = false;
-					break;
-				}
-			}
-			if (flag) count++;
-		}
+		let row = arrayToString(grid[i]);
+		map.set(row, (map.get(row) || 0) + 1);
+	}
+
+	for (let j = 0; j < grid.length; j++) {
+		let col = arrayToString(grid.map(row => row[j]));
+		count += map.get(col) || 0;
 	}
 	return count;
 }
