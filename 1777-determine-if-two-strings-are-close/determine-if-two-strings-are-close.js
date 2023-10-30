3f517cdaf2c97b45f32fe431eac2737eafdfb557
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-10-30 17:45:46 +0400

    Time: 122 ms (84.18%) | Memory: 48.5 MB (91.41%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.js
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.js
@@ -4,22 +4,25 @@
  * @return {boolean}
  */
 function closeStrings(word1, word2) {
-	let set1 = new Set(word1);
-	let set2 = new Set(word2);
+	const getFrequencyAndSet = word => {
+		const set = new Set();
+		const freq = Array.from({ length: 26 }, _ => 0);
+
+		for (let char of word) {
+			set.add(char);
+			freq[char.charCodeAt() - 97]++;
+		}
+
+		return [set, freq.sort((a, b) => a - b)];
+	};
+
+	const [set1, freq1] = getFrequencyAndSet(word1);
+	const [set2, freq2] = getFrequencyAndSet(word2);
+
 	if (set1.size !== set2.size || [...set1].sort().join("") !== [...set2].sort().join("")) return false;
-
-	let freq1 = Array.from({ length: 26 }, _ => 0),
-		freq2 = Array.from({ length: 26 }, _ => 0);
-
-	for (let char of word1) freq1[char.charCodeAt() - 97]++;
-	for (let char of word2) freq2[char.charCodeAt() - 97]++;
-
-	freq1.sort((a, b) => a - b);
-	freq2.sort((a, b) => a - b);
 
 	for (let i = 0; i < freq1.length; i++) {
 		if (freq1[i] !== freq2[i]) return false;
 	}
 	return true;
 }
-
