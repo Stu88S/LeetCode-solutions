af82e8c3c3145ccec3b21f7d7938a9fa167d8b2c
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-10-31 19:49:00 +0400

    Time: 68 ms (28.76%) | Memory: 44.6 MB (11.04%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.js
+++ 735-asteroid-collision/asteroid-collision.js
@@ -4,20 +4,25 @@
  */
 function asteroidCollision(asteroids) {
 	let stack = [];
+
+	const pushAsteroid = asteroid => {
+		let absAsteroid = Math.abs(asteroid);
+		while (stack.length > 0 && stack[stack.length - 1] < absAsteroid && stack[stack.length - 1] > 0) {
+			stack.pop();
+		}
+		if (stack.length === 0 || stack[stack.length - 1] < 0) {
+			stack.push(asteroid);
+		} else if (stack[stack.length - 1] === absAsteroid) {
+			stack.pop();
+		}
+	};
+
 	for (let i = 0; i < asteroids.length; i++) {
 		let asteroid = asteroids[i];
 		if (asteroid > 0) {
 			stack.push(asteroid);
 		} else {
-			let absAsteroid = Math.abs(asteroid);
-			while (stack.length > 0 && stack[stack.length - 1] < absAsteroid && stack[stack.length - 1] > 0) {
-				stack.pop();
-			}
-			if (stack.length === 0 || stack[stack.length - 1] < 0) {
-				stack.push(asteroid);
-			} else if (stack[stack.length - 1] === absAsteroid) {
-				stack.pop();
-			}
+			pushAsteroid(asteroid);
 		}
 	}
 	return stack;
