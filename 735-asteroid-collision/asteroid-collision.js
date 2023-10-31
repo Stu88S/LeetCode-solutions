693071e7aeee7c076740e37e63557b88b56f7f9b
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-10-31 19:48:10 +0400

    Time: 54 ms (89.67%) | Memory: 44.1 MB (38.29%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.js
+++ 735-asteroid-collision/asteroid-collision.js
@@ -3,20 +3,22 @@
  * @return {number[]}
  */
 function asteroidCollision(asteroids) {
-    let stack = [];
-    for(let i = 0; i < asteroids.length; i++) {
-        if(asteroids[i] > 0) {
-            stack.push(asteroids[i]);
-        } else {
-            while(stack.length > 0 && stack[stack.length - 1] > 0 && stack[stack.length - 1] < Math.abs(asteroids[i])) {
-                stack.pop();
-            }
-            if(stack.length === 0 || stack[stack.length - 1] < 0) {
-                stack.push(asteroids[i]);
-            } else if(stack[stack.length - 1] === Math.abs(asteroids[i])) {
-                stack.pop();
-            }
-        }
-    }
-    return stack;
+	let stack = [];
+	for (let i = 0; i < asteroids.length; i++) {
+		let asteroid = asteroids[i];
+		if (asteroid > 0) {
+			stack.push(asteroid);
+		} else {
+			let absAsteroid = Math.abs(asteroid);
+			while (stack.length > 0 && stack[stack.length - 1] < absAsteroid && stack[stack.length - 1] > 0) {
+				stack.pop();
+			}
+			if (stack.length === 0 || stack[stack.length - 1] < 0) {
+				stack.push(asteroid);
+			} else if (stack[stack.length - 1] === absAsteroid) {
+				stack.pop();
+			}
+		}
+	}
+	return stack;
 }
