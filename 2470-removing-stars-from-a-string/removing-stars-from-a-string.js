fd25767c701eab8f944f3ed23340a8d6974ed394
Andrei Larchenkov <stu88s.tor@gmail.com>
2023-10-31 19:40:52 +0400

    Time: 111 ms (20.00%) | Memory: 54.2 MB (96.00%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.js
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.js
@@ -3,13 +3,15 @@
  * @return {string}
  */
 function removeStars(s) {
-	let stack = [];
-	for (let char of s) {
-		if (char === "*") {
-			stack.pop();
-		} else {
-			stack.push(char);
-		}
-	}
-	return stack.join("");
+    let arr = Array.from(s);
+    let j = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '*') {
+            j = Math.max(0, j - 1);
+        } else {
+            arr[j] = arr[i];
+            j++;
+        }
+    }
+    return arr.slice(0, j).join('');
 }
